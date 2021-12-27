3eb6178b66f04488724122c0b668a64496f01ffa
Evan Beck <evandrydenbeck@gmail.com>
2021-12-27 16:33:31 -0500

    commit tree 084f57196a5e4501007be38f644271764154d270

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log('HELLO');
+    return string.toUpperCase();
+}
+function logWhisper(string) {
+    console.log('hello');
+    return string.toLowerCase();
+}
+const string = "Let's have dinner together!"
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase {
+    return "I can\'t hear you."}
+}
